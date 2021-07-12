8063d28a25597448b279bc2d8338908136d238ce
oelgoubi <othmanelgoubi@gmail.com>
2021-07-12 23:24:15 +0200

    add refs to variable to make them reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello</h1>
+    <p>My NAME is {{name}} and my age is {{age}}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue"
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  // Setup Hook That execute before all the cycle hooks
+  // This keyword is not available inside the Setup Hook
+  setup(){
+    console.log('setup');
+    //const p = ref(null);// p.value will be undefined untill we return it from the setup function
+    // Ref is const but the value can change
+    const name = ref("Sami"), age = ref(12);
+
+    const handleClick = ()=>{
+      name.value = "Othy"
+      age.value = 22
+    }
+
+    return { name, age,handleClick }
+
   }
 }
 </script>
