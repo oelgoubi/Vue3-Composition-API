841a2fdcc102f96f74e4fade29f5369fe5c8531b
oelgoubi <othmanelgoubi@gmail.com>
2021-07-12 23:37:52 +0200

    usage of refs and reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
-    <h1>Hello</h1>
-    <p>My NAME is {{name}} and my age is {{age}}</p>
+    <h1>REF</h1>
+    <p>My NAME is {{ninja.name}} and my age is {{ninja.age}}</p>
     <button @click="handleClick">Click me</button>
+    <button @click="ninja.age++">Increase age</button>
+    <input type="text" v-model="ninja.name">
+    <h1>Reactive</h1>
+    <p>My NAME is {{ninja2.name}} and my age is {{ninja2.age}}</p>
+    <button @click="handleClick2">Click me</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue"
+import { ref,reactive } from "vue"
 export default {
   name: 'Home',
   // Setup Hook That execute before all the cycle hooks
@@ -16,14 +21,19 @@ export default {
     console.log('setup');
     //const p = ref(null);// p.value will be undefined untill we return it from the setup function
     // Ref is const but the value can change
-    const name = ref("Sami"), age = ref(12);
+    const ninja = ref({ name:'mario', age: 24 });
+    const ninja2 = reactive({ name:'Othy', age: 12 });// We can't use it with primitve values
 
     const handleClick = ()=>{
-      name.value = "Othy"
-      age.value = 22
+      ninja.value.name = "Othy"
+      ninja.value.age = 22
     }
 
-    return { name, age,handleClick }
+    const handleClick2 = ()=>{
+      ninja2.age = 69
+    }
+
+    return {ninja,handleClick,handleClick2,ninja2 }
 
   }
 }
